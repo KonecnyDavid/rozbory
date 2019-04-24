b31336de3100a857df170d14477b6689846f047d
Denis Homolík <denis@homolik.cz>
2019-04-24 16:22:39 +0200

    Add compression

--- server/index.ts
+++ server/index.ts
@@ -1,6 +1,7 @@
 import * as next from "next";
 import * as express from "express";
 import api from "./api";
+import * as compression from "compression";
 
 const app = next({
   dev: process.env.NODE_ENV !== "production",
@@ -11,6 +12,7 @@ const handler = app.getRequestHandler();
 
 app.prepare().then(() => {
   express()
+    .use(compression())
     .get("/api*", (req, res) => api(req, res))
     .use((req, res) => handler(req, res))
     .addListener("error", () => (err: Error) => {
